887750e9df6ccf83144d815f0b4cbed9c90eca9d
bakabie <blackguwc@163.com>
2019-06-09 00:56:05 +0800

    画饼好难

--- world.py
+++ world.py
@@ -10,17 +10,26 @@ class TheWorkd(object):
         users_hash =[]
         # self.time = None 想了想似乎不需要？
         self.round = 0
+        self.flag_gameend = False
+        self.alive_user = user
         self.event_list = []
         return users_hash
+    def __checkAliveUser(self):
+        # 判断存活玩家
+        if self.alive_user == 1:
+            self.flag_gameend = True
+            # 游戏结束
+            return
     def __roundIteration(self):
-    """
-        游戏回合迭代，回合制 or rts？ 
-    """
+        #游戏回合迭代 
         pass
-    def __createWorld(self,):
+    def __createWorld(self):
         # 地图创建
         pass
 
     def Api_Move(self,user_hash,direction,l):
+        # 控制某个目标移动
         pass 
-        
+    def Api_Build(self,user_hash,obj_id,createdobj_id,xyz=()):
+        # 控制某个目标建造某个东西
+        pass
