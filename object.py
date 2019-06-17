4666b356e2b84f57e0f3126a526259df443d381b
bakabie <blackguwc@163.com>
2019-06-18 01:32:59 +0800

    龟速填坑 + 添加了一些object + 稍微让目标清晰了点 今天芭蕾舞挂科了不开心，要期末考了没心思填坑

--- object.py
+++ object.py
@@ -1,6 +1,55 @@
-class Me(object):
+# todo:创建随机属性
+# todo:完善基本对象接口需求
+
+
+class ObjectNotFound:
+    pass
+
+
+class BaseObject(object):
+    pass
+
+
+class T(object):
     def __init__(self):
-        self.id = 1
+        self.maps = {}
+
+    def search(self, id):
+        if id in self.maps:
+            return self.maps[id]
+        else:
+            raise ObjectNotFound
+
+
+class Me(object):
+    def __init__(self, id):
+        self.type_id = 1
+        self.id = id
+        self.ATK = 0
+        self.bag = {}
+        self.DEF = 0
+
+
+    def get_type(self):
+        return self.type_id
+
+    def attributes(self):
+        pass
 
     def action(self):
         pass
+
+
+class Planet(object):
+    def __init__(self, id):
+        self.type_id = 2
+        self.id = id
+        self.store = 10000
+        self.water = 10000
+
+    def get_type(self):
+        return self.type_id
+
+    def action(self):
+        pass
+
--- world.py
+++ world.py
@@ -1,13 +1,18 @@
 # coding:utf-8s
-class TheWorld(object):
+import random,uuid
 
-    def __init__(self, user=0, size=(100, 100, 100)):
+
+class TheWorld(object):
+    # todo:完善新对象的生存周期的相关操作，例如对象生成/移动/销毁等
+    def __init__(self, size=(100, 100, 100)):
         super(TheWorld, self).__init__()
         # self.time = None 想了想似乎不需要？
         self.round = 0
         self.flag_finish = False
-        self.alive_user = user
-        self.event_list = []
+        self.alive_user = 0
+        self.objects = {}
+        self.world_size = size
+        self.__create_world(size[0], size[1], size[2])
 
     def __check_alive_user(self):
         # 判断存活玩家
@@ -20,13 +25,25 @@ class TheWorld(object):
         # 游戏回合迭代
         pass
 
-    def __create_world(self):
+    def __create_world(self, x, y, z):
         # 地图创建
-        pass
+        self.world = [[[0] * x for i in range(y)] * x for i in range(z)]  # 构建3D世界
+
+    def __user_generate(self):
+        x = random.randint(0, self.world_size[0])
+        y = random.randint(0, self.world_size[1])
+        z = random.randint(0, self.world_size[2])
+        user_hash = uuid.uuid4()
+        return user_hash
+
+    def control_add_user(self):
+        user_hash = self.__user_generate()
+        return user_hash
 
     def api_move(self, user_hash, obj_id, direction_x_y_z):
         # 控制某个目标移动
         pass
 
     def api_action(self, user_hash, obj_id, direction_x_y_z, action_type):
+        # todo:完善事件ID和事件对应的参数表
         pass
